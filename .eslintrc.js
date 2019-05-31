7149dbf7c8b93a7e8c56b33d0eb36f51599e2153
VPARAMO <vparamo@ford.com>
2019-05-31 10:30:24 -0500

    Dependencies added

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "linebreak-style": 0,
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+  },
+  "parser": "babel-eslint"
+};
